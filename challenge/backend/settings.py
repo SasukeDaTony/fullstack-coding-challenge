1448e78cc22b58054d24c52619e1852277df2f3e
SasukeDaTony <tonytaylor@pursuit.org>
2024-04-12 16:55:18 -0400

    updating settings files , allowed hosts and db

--- challenge/backend/settings.py
+++ challenge/backend/settings.py
@@ -9,12 +9,13 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-
+import dj_database_url
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# postgres://djg_db_user:fSoGIRbAW1nbrrucjWu5yWazZRsj5XI3@dpg-cocp3e21hbls73cvcs1g-a.oregon-postgres.render.com/djg_db
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -25,7 +26,7 @@ SECRET_KEY = "#m8p(q=10bq+kl#!huob(2$w4gp#&b&hv3d=$%ma+c)8+4syo%"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://fullstack-coding-challenge-web.onrender.com"]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
@@ -83,7 +84,12 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
+    },
+    "default": dj_database_url.parse(
+        "postgres://djg_db_user:fSoGIRbAW1nbrrucjWu5yWazZRsj5XI3@dpg-cocp3e21hbls73cvcs1g-a.oregon-postgres.render.com/djg_db",
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
 }
 
 
